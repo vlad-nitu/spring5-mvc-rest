7a921b85c7a152623c75f7890c27528132312242
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-08-03 14:10:32 +0300

    Addedd MapStruct to project
    Took 7 minutes

---
+++ src/test/java/guru/springfamework/mapper/CategoryMapperTest.java
@@ -0,0 +1,25 @@
+package guru.springfamework.mapper;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.domain.Category;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryMapperTest {
+    public static final String NAME = "Joe";
+    public static final long ID = 1L;
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    @Test
+    public void categoryToCategoryDTO() throws Exception{
+        Category category = new Category();
+        category.setId(ID);
+        category.setName(NAME);
+
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        assertEquals(Long.valueOf(ID), categoryDTO.getId());
+        assertEquals(NAME, categoryDTO.getName());
+    }
+}
