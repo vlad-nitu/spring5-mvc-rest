7236918e2764c934958ca415b110655c97bdd845
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-08-03 16:54:54 +0300

    Added PUT of a Customer functionality
    Took 8 minutes

--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -18,10 +18,12 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerMapper customerMapper;
     private CustomerRepository customerRepository;
+
     @Autowired
     public void setCustomerMapper(CustomerMapper customerMapper) {
         this.customerMapper = customerMapper;
     }
+
     @Autowired
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
@@ -33,9 +35,9 @@ public class CustomerServiceImpl implements CustomerService {
                 .findAll()
                 .stream()
                 .map(customer -> {
-                   CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
-                   customerDTO.setCustomerUrl("/api/v1/customers/" + customer.getId());
-                   return customerDTO;
+                    CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+                    customerDTO.setCustomerUrl("/api/v1/customers/" + customer.getId());
+                    return customerDTO;
                 })
                 .collect(Collectors.toList());
     }
@@ -49,20 +51,34 @@ public class CustomerServiceImpl implements CustomerService {
                     customerDTO.setCustomerUrl("/api/v1/cusomer/" + id);
                     return customerDTO;
                 })
-                .orElseThrow(RuntimeException::new); //todo implement better exception handling
+                .orElseThrow(RuntimeException::new); //TODO: implement better exception handling
     }
 
     @Override
     public CustomerDTO createNewCustomer(CustomerDTO customerDTO) {
 
         Customer customer = customerMapper.customerDtoToCustomer(customerDTO);
+        return saveAndReturnDTO(customer);
+    }
 
+    @Override
+    public CustomerDTO saveCustomerByDTO(Long id, CustomerDTO customerDTO) {
+        Customer customer = customerMapper.customerDtoToCustomer(customerDTO);
+        customer.setId(id);
+        return saveAndReturnDTO(customer);
+    }
+
+    /**
+     * Utility method that saves and returns a Data Transfer Object
+     *
+     * @param customer Customer object that needs to be saved and returned as a CustomerDTO object (after being converted
+     *                 using CustomerMapping)
+     * @return CustomerDTO object representing the Customer object that was saved & returned
+     */
+    private CustomerDTO saveAndReturnDTO(Customer customer) {
         Customer savedCustomer = customerRepository.save(customer);
-
         CustomerDTO returnDto = customerMapper.customerToCustomerDTO(savedCustomer);
-
         returnDto.setCustomerUrl("/api/v1/customer/" + savedCustomer.getId());
-
         return returnDto;
     }
 }
